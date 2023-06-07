cfeb814e91956793c4dc5a1f54be466030c748ef
Anjali <anjalimohta445@gmail.com>
2023-06-07 13:31:24 -0400

    card code added

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FoodServiceService } from 'src/app/Services/food-service.service';
+import { Foods } from 'src/app/models/food';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  foods: Foods[] = [];
+
+  constructor(private fs:FoodServiceService){}
+
+  ngOnInit(): void {
+    this.foods = this.fs.getFoodData();
+  }
 
 }
