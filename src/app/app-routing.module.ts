4925caf39cfe2c9c8ab041e8cd507f03a228290d
Anjali <anjalimohta445@gmail.com>
2023-06-07 03:52:19 -0400

    material and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { SearchComponent } from './Components/search/search.component';
+import { CartComponent } from './Components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'cart', component:CartComponent},
+  {path:'search', component:SearchComponent},
+  {path:'**', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
